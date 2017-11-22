5719fe5b7c67a967f77d360b9415e78c294e6587
rajeshgupta3579 <rajeshgupta3579@gmail.com>
2017-11-22 23:31:30 +0000

    Geohash Stuff

---
+++ src/Exercise/JsonUtil.java
@@ -0,0 +1,85 @@
+package Exercise;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+  
+public class JsonUtil {
+	
+	public static final String NO_DATA = "{\"data\":null}";
+	public static final String NO_RESULT = "{\"result\":false}";
+	private static ObjectMapper mapper;
+	static{
+		mapper = new ObjectMapper();
+		mapper.setSerializationInclusion(Include.NON_NULL);
+	}
+	
+	public static JsonNode josn2Object(String json){
+		 try {
+			return mapper.readTree(json);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block  
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block  
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public static String parseJson(Object obj){
+		
+		if(obj == null){
+			return NO_DATA;
+		}
+		
+		try {
+			return mapper.writeValueAsString(obj);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block  
+			e.printStackTrace();
+			return NO_DATA;
+		}
+	}
+	
+	public static String parseJson(Object obj, String root){
+		
+		if(obj == null){
+			return NO_DATA;
+		}
+		
+		try {
+			StringBuilder sb = new StringBuilder();
+			sb.append("{\"");
+			sb.append(root);
+			sb.append("\":");
+			sb.append(mapper.writeValueAsString(obj));
+			sb.append("}");
+			return sb.toString();
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return NO_DATA;
+		}
+	}
+	
+	public static String wrapperJsonp(String json, String var){
+		if(var == null){
+			var = "datas";
+		}
+		return new StringBuilder().append("var ").append(var).append("=").append(json).toString();
+	}
+	
+	public static void main(String[] args) {
+		HashMap<String, Integer> hash = new HashMap<String, Integer>();
+		hash.put("key1", 1);
+		hash.put("key2", 2);
+		hash.put("key3", 3);
+		System.out.println(JsonUtil.parseJson(hash));
+	}
+}
---
+++ src/Exercise/Location.java
@@ -0,0 +1,27 @@
+package Exercise;
+
+public class  Location {   
+    public static final double  MINLAT = - 90 ;     
+    public static final double  MAXLAT =  90 ;     
+    public static final double  MINLNG = - 180 ;     
+    public static final double  MAXLNG =  180 ;     
+    private double  lat; // latitude [-90,90]   
+    private double  lng; // Longitude [-180,180]   
+      
+    public  Location ( double  lat,  double  lng) {  
+        this .lat = lat;  
+        this .lng = lng;  
+    }  
+    public double  getLat () {   
+        return  lat;  
+    }  
+    public void  setLat ( double  lat) {   
+        this .lat = lat;  
+    }  
+    public double  getLng () {   
+        return  lng;  
+    }  
+    public void  setLng ( double  lng) {   
+        this .lng = lng;  
+    }  
+}  
